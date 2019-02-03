96f03a57f7cda3988011b9ebf4318d4e7aabb4d9
Hong Yi Xiong <xionghongyi@gmail.com>
2019-02-03 17:46:40 -0500

    - Added comments for functions to be implemented.

--- src/nqueens.py
+++ src/nqueens.py
@@ -9,21 +9,69 @@ class NQueens:
         self.num_conflicts = []
 
     def nqueens_min_conflicts_iterative_repair(self):
-        return None
+        """
+        Uses iterative repair method along with the min-conflicts heuristic to find a solution to the N-Queens problem.
 
-    def min_conflicts(self):
-        pass
+        :return: a list representing a solution to the N-Queens problem
+        """
+        self.generate_initial_positions()
 
-    def has_conflict(self):
+        # todo: remove setting data created for testing purpose
+        self.positions = [[1, 2, 3, 4], [-2, 3, 63, 5, 8], [], [4, 0, 1, 5, -1]]
+        return self.positions
+
+    def min_conflicts(self, row):
+        """
+        Find a square with minimum number of conflicts on the given row number.
+
+        :param row:
+        :return: the destination column number of where the queen should be moved to,
+                  the list of row numbers where their conflict numbers need to be reduced by 1, and
+                  the list of row numbers where their conflict numbers need to be increased by 1.
+        """
+        destination_col = 0  # todo: to be replaced
+        delete_list = []
+        update_list = []
+
+        return destination_col, delete_list, update_list
+
+    def has_conflict(self, row1, col1, row2, col2):
+        """
+        Determines whether two queens on the given row numbers and column numbers will have a conflict.
+
+        :param row1: row number of first square
+        :param col1: column number of first square
+        :param row2: row number of second square
+        :param col2: column number of second square
+        :return: a boolean indicating whether queens on these two squares will have a conflict
+        """
         return True
 
-    def initialize_positions(self):
-        return []
+    def generate_initial_positions(self):
+        """
+        Populate self.positions with values representing queens columns.
+        """
+        pass
 
     def move_queen(self, source_row, destination_col, delete_list, update_list):
+        """
+        Moves a queen on the given row number to the destination column on the same row.
+        The function also updates the num_conflicts list.
+
+        :param source_row: the row number of the queen to be moved
+        :param destination_col: the col number of the queen to be moved to on the source_row
+        :param delete_list: the list of row numbers where their conflict numbers need to be reduced by 1
+        :param update_list: the list of row numbers where their conflict numbers need to be increased by 1
+        """
         pass
 
     def find_max(self):
+        """
+        Find the queen with the maximum number of conflicts. If there are more than one queen with maximum number of conflicts,
+        then choose a queen randomly.
+
+        :return: a row number representing the queen
+        """
         return None
 
 
@@ -35,10 +83,12 @@ def read_file_to_list(file_name):
 
 
 def write_2d_list_to_file(data, file_name):
-    # todo: remove setting data created for testing purpose
-    data = [[1, 2, 3, 4], ['a', 3, 'be', 5, 8], [], [4, 9, 1, 5, '2faf']]
     file = open(file_name, "w+")
     for row in range(0, len(data)):
+        # todo: might need to add 1 to x because row and column numbers on a chess board
+        # are 1 higher than Python list indices.
+        # That is:
+        # new_line = '[' + ''.join([(str(x+1) + ',') for x in data[row]]).rstrip(',') + ']'
         new_line = '[' + ''.join([(str(x) + ',') for x in data[row]]).rstrip(',') + ']'
         file.write(new_line + '\n')
     file.close()
@@ -52,8 +102,8 @@ def main():
 
     results = []
     for n in input_data:
-        result = NQueens(n)
-        results.append(result.nqueens_min_conflicts_iterative_repair())
+        nqueens = NQueens(n)
+        results.append(nqueens.nqueens_min_conflicts_iterative_repair())
 
     write_2d_list_to_file(results, output_file_name)
 
